462e73bcc2d2eb20ec466f76830c355b49c7b09e
Mike Adams <34733342+therealmikey@users.noreply.github.com>
2021-11-03 13:51:00 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+<?php echo (isset($_GET['title'])) ? $_GET['title'] : 'The Index'?>
+</title>
+</head>
+<body>
+<h1>
+<?php echo (isset($_GET['title'])) ? $_GET['title'] : 'The Index'?>
+</h1>
+<p>All of Mikey's recipes of chaos</p>
+<i>Use 'ctrl' + 'f' to find recipes</i>
+<hr>
+<h2>
+<?php echo (isset($_GET['gp'])) ? $_GET['gp'] : 'Combustion Dust'?>
+</h2>
+<p>How to make a basic gunpowder. This product could be utilized for many different uses.</p>
+<h3>Ingredients:</h3>
+<ul>
+    <li>Potassium Nitrate</li>
+    <li>Carbon</li>
+    <li>Sulfur</li>
+</ul>
+<p>Potassium Nitrate: 75%; AKA Saltpeter, Stump Remover, or Niter</p>
+<p>Carbon: 15%; Get it from charcoal</p>
+<p>Sulfur: 10%; Elemental Sulfur can be obtained from gardening stores (used for making soil more acidic)</p>
+<p>Mixing tips:</p>
+<ul>
+    <li>Use caution (you're making gunpowder!)</li>
+    <li>Consider adding water? If you do, break it up into small chunks!</li>
+</ul>
+<b>Notes:</b>
+<ul>
+    <li>Store safely!</li>
+    <li>Highly combustable!</li>
+</ul>
+<hr>
+<h2>Sparkling combustion dust</h2>
+<p>Using basic gunpowder to make very, very bright flares of white light. This is also good for a "falling star" effect in fireworks/rockets.</p>
+<h3>Ingredients:</h3>
+<ul>
+    <li>Gunpowder</li>
+    <li>Magnesium</li>
+</ul>
+<p>Magnesium: Add in flakes; Magnesium is the silver looking part of flint and steel</p>
+<b>Notes:</b>
+<ul>
+    <li>This may cause some blindness because of the brightness</li>
+    <li>In fireworks/rockets this creates a "falling star" effect that looks pretty</li>
+</ul>
+<hr>
+<h2>Clay</h2>
+<p>How to make a very useful material that can be used to make anything you mold it into</p>
+<h3>Ingredients:</h3>
+<ul>
+    <li>Dirt</li>
+    <li>Water</li>
+    <li>Strainer</li>
+    <li>Bucket</li>
+</ul>
+<p>Dirt: Any dirt near bodys of water will work</p>
+<p>Water: Clean water</p>
+<p>Strainer: A piece of cloth to seperate organic debris; A second, much thicker cloth (dense towel) to hold the liquid and let it dry out</p>
+<p>Bucket: Mix water and dirt 3:1; Hold this liquid inside of the thickest cloth to tie off the top</p>
+<b>Notes:</b>
+<ul>
+    <li>The 3:1 (water to dirt) liquid must dry out inside the thick cloth</li>
+    <li>Once the water evaporates from the 3:1 solution, take the clay and mold it into the desired shape</li>
+</ul>
+<hr>
+</body>
+</html>
+
